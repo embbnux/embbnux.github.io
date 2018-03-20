2875d7f1903a88358af2be79ee1b85fdad63f0b3
embbnux <embbnux@embbnux.com>
2018-03-20 16:50:48 +0800

    fix bug

--- ringcentral-web-widget/adapter.js
+++ ringcentral-web-widget/adapter.js
@@ -6,5 +6,5 @@
   var rc_s = document.createElement("script");
   rc_s.src = (currentScipt && currentScipt.src) || "https://embbnux.github.io/ringcentral-embeddable-voice/adapter.js";
   var rc_s0 = document.getElementsByTagName("script")[0];
-  rc_s0.parentNode.insertBefore(rc_s, rcs0);
+  rc_s0.parentNode.insertBefore(rc_s, rc_s0);
 })();
