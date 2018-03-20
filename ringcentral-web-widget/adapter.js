e3acccc35e6cee518f9d7db10d0f34de1be53b9b
embbnux <embbnux@embbnux.com>
2018-03-20 23:33:55 +0800

    fix typo

--- ringcentral-web-widget/adapter.js
+++ ringcentral-web-widget/adapter.js
@@ -1,16 +1,17 @@
 (function() {
-  var currentScipt = document.currentScript;
-  if (!currentScipt) {
-    currentScipt = document.querySelector('script[src*="adapter.js"]');
+  console.warn('this uri is deprecated. Please stop use it soon before the feature is completely removed. Please use new uri: https://ringcentral.github.io/ringcentral-embeddable-voice/adapter.js');
+  var currentScript = document.currentScript;
+  if (!currentScript) {
+    currentScript = document.querySelector('script[src*="adapter.js"]');
   }
-  if (currentScipt && currentScipt.src) {
-    currentScipt = currentScipt.src;
-    currentScipt = currentScipt.replace('ringcentral-web-widget', 'ringcentral-embeddable-voice');
+  if (currentScript && currentScript.src) {
+    currentScript = currentScript.src;
+    currentScript = currentScript.replace('ringcentral-web-widget', 'ringcentral-embeddable-voice');
   } else {
-    currentScipt = "https://embbnux.github.io/ringcentral-embeddable-voice/adapter.js";
+    currentScript = "https://embbnux.github.io/ringcentral-embeddable-voice/adapter.js";
   }
   var rc_s = document.createElement("script");
-  rc_s.src = currentScipt;
+  rc_s.src = currentScript;
   var rc_s0 = document.getElementsByTagName("script")[0];
   rc_s0.parentNode.insertBefore(rc_s, rc_s0);
 })();
