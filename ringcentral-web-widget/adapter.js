10bde834991c3bf762bfde82609b082c83e8e830
embbnux <embbnux@embbnux.com>
2018-03-20 16:48:32 +0800

    fix redirect bug

--- ringcentral-web-widget/adapter.js
+++ ringcentral-web-widget/adapter.js
@@ -4,7 +4,7 @@
     currentScipt = document.querySelector('script[src*="adapter.js"]');
   }
   var rc_s = document.createElement("script");
-  rc_s.src = currentScipt || "https://embbnux.github.io/ringcentral-embeddable-voice/adapter.js";
+  rc_s.src = (currentScipt && currentScipt.src) || "https://embbnux.github.io/ringcentral-embeddable-voice/adapter.js";
   var rc_s0 = document.getElementsByTagName("script")[0];
   rc_s0.parentNode.insertBefore(rc_s, rcs0);
 })();
